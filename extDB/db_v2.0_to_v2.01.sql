e49d6c0d0a48737dc968593ddbb9e8d67fd6da49
AgentRev <a456197@gmail.com>
2014-12-14 13:05:57 -0500

    DB mini-transition to v2.01

---
+++ extDB/db_v2.0_to_v2.01.sql
@@ -0,0 +1,13 @@
+-- This is for those who setup the database using the initial v2.0 script
+
+START TRANSACTION;
+USE a3wasteland;
+
+ALTER TABLE PlayerInfo 
+CHANGE COLUMN Name Name VARCHAR(256) CHARACTER SET 'utf8' NULL,
+ADD COLUMN AdminNote VARCHAR(4096) CHARACTER SET 'utf8' NULL;
+
+INSERT INTO DBInfo SET Name = 'Version', Value = '2.01' 
+ON DUPLICATE KEY UPDATE Value = VALUES(Value);
+
+COMMIT;
